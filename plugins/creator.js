e1e0c18b6e4ff9e7d0814dd2cae72f3f56b71b0b
NdaaXD <98279739+NdaaXD@users.noreply.github.com>
2022-03-12 15:58:51 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 const PhoneNumber = require('awesome-phonenumber')
 async function handler(m) {
-                let vcard = "BEGIN:VCARD\nVERSION:3.0\nN:1;NdaBot-MD;;;\nFN:NdaBot-MD\nitem1.TEL;waid=6288220118685:+62 882-2011-8685\nitem1.X-ABLabel:Ponsel\nX-WA-BIZ-NAME:NdaaBotz-MD\nEND:VCARD"
+                let vcard = "BEGIN:VCARD\nVERSION:3.0\nN:1;;Fito;;\nFN:Fito\nitem1.TEL;waid=6285860278865:+62 858-6027-8865\nitem1.X-ABLabel:Ponsel\nX-WA-BIZ-NAME:NdaaBotz-MD\nEND:VCARD"
                 conn.sendMessage(m.chat, { contacts: { contacts: [{ vcard }] } }, { quoted: m })
 }
 handler.help = ['owner', 'creator']
